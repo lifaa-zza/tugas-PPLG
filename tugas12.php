6b5edaa7b5280afad7cf751dc785c0b32463f1e7
lifaa-zza <alifahizzahr@gmail.com>
2025-05-15 15:33:59 +0800

    tugas

---
+++ tugas12.php
@@ -0,0 +1,23 @@
+<?php
+
+$Daftar_Anggota_Kelas10B= [   
+    "Akifah Zahrani Arafat",
+    "Al Qumaerah",
+    "Althaf Azzah Zahra",
+    "Andi Al Firqahtunnajiyah Ahsyar",
+    "Andi Mufrodatul Fanzila",
+    "Fahrah Furaidah Sahrir",
+    "Fauziyyah",
+    "Jillan Fathiyyah",
+    "Nadia Mirzayanti Hasbi",
+    "Rabiatul Adawiyah",
+    "Alifah Izzah Rahmadini",
+    "Muftihaturrahmah",
+    "Auliyah Nastitiy Khayla Armada",
+    "Putri Kinanti Al Magfirah",
+] ;
+echo "Daftar Anggota Kelas 10 B</br>";
+    foreach ($Daftar_Anggota_Kelas10B as $daftar_Anggota) {
+        
+        echo "-" .$daftar_Anggota ."</br>";
+}
